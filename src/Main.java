53aba2f04b69407eb9401a4cc68889f88cd1fe4b
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-12-09 17:44:56 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		String dia;
+
+		if (x == 1) {
+			dia = "Domingo";
+		} else if (x == 2) {
+			dia = "Segunda";
+		} else if (x == 3) {
+			dia = "Terça";
+		} else if (x == 4) {
+			dia = "Quarta";
+		} else if (x == 5) {
+			dia = "Quinta";
+		} else if (x == 6) {
+			dia = "Sexta";
+		} else if (x == 7) {
+			dia = "Sábado";
+		} else {
+			dia = "Valor inválido";
+		}
+
+		System.out.println("Dia da semana: " + dia);
+		sc.close();
+	}
+}
